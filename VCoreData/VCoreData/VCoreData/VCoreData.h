82e0204dcd9cf95a1091cb8ff013998b49ba393e
abcxo <282325694@qq.com>
2014-03-12 15:23:43 +0800

    Initial Commit

---
+++ VCoreData/VCoreData/VCoreData/VCoreData.h
@@ -0,0 +1,13 @@
+//
+//  VCoreData.h
+//  VCoreData
+//
+//  Created by shadow on 14-3-12.
+//  Copyright (c) 2014年 SJ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VCoreData : NSObject
+
+@end
---
+++ VCoreData/VCoreData/VCoreData/VCoreData.m
@@ -0,0 +1,13 @@
+//
+//  VCoreData.m
+//  VCoreData
+//
+//  Created by shadow on 14-3-12.
+//  Copyright (c) 2014年 SJ. All rights reserved.
+//
+
+#import "VCoreData.h"
+
+@implementation VCoreData
+
+@end
