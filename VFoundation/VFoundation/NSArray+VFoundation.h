d99d9c938cde1c5dea5ba5a8a6b8473635e3e0cd
yongry <527068687@qq.com>
2014-03-15 12:20:51 +0800

    VNSArray

--- VFoundation/VFoundation/NSArray+VFoundation.h
+++ VFoundation/VFoundation/NSArray+VFoundation.h
@@ -7,6 +7,8 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <objc/runtime.h>
+
 /**
  *  NSArray' category in VFoundation
  */
--- VFoundation/VFoundation/NSArray+VFoundation.m
+++ VFoundation/VFoundation/NSArray+VFoundation.m
@@ -10,4 +10,51 @@
 
 @implementation NSArray (VFoundation)
 
+/**
+ *  Method swizzling for NSArray
+ */
++ (void)load {
+	[super load];
+	Class arrayClass = NSClassFromString(@"__NSArrayM");
+
+	Method originalMethod = class_getInstanceMethod(arrayClass, @selector(addObject:));
+	Method categoryMethod = class_getInstanceMethod([NSArray class], @selector(addObjectSafe:));
+	method_exchangeImplementations(originalMethod, categoryMethod);
+
+	originalMethod = class_getInstanceMethod(arrayClass, @selector(objectAtIndex:));
+	categoryMethod = class_getInstanceMethod([NSArray class], @selector(objectAtIndexSafe:));
+	method_exchangeImplementations(originalMethod, categoryMethod);
+}
+
+/**
+ *  to protect the array from inserting nil object
+ *
+ *  @param voidaddObjectSafe:id : the object needed to be inserted
+ *
+ *  @return void
+ */
+- (void)addObjectSafe:(id)anObject {
+	if (anObject != nil) {
+		[self addObjectSafe:anObject];
+	}
+}
+
+/**
+ *  to protect the array from accessing object at index beyond the array
+ *
+ *  @param index : the index of the object
+ *
+ *  @return the valid object at index or nil
+ */
+- (id)objectAtIndexSafe:(NSUInteger)index {
+	id obj = nil;
+	if (index < [self count]) {
+		obj = [self objectAtIndex:index];
+	}
+	else {
+		NSLog(@"数组越界");
+	}
+	return obj;
+}
+
 @end
