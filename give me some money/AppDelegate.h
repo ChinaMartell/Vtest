61e6b3a4ff7d76f5691081e1aae79f466c080921
abcxo <282325694@qq.com>
2014-03-20 15:55:09 +0800

    给我点钱，测试app

---
+++ give me some money/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  give me some money
+//
+//  Created by shadow on 14-3-20.
+//  Copyright (c) 2014年 SJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
